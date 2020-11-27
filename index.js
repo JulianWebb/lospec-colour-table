7cb0451369036832f0673228404cb84eeeca777d
Julian Webb <git@julianwebb.ca>
2020-11-27 00:37:17 -0800

    Further refinement

--- index.js
+++ index.js
@@ -14,14 +14,12 @@ app.get('/:palette.act', (req, res) => {
     fetch(`https://lospec.com/palette-list/${palette}.json`)
         .then(fetchRes => fetchRes.json())
         .then(json => {
-            let colors = Buffer.alloc(json.colors.length * 3, json.colors.reduce((acc, cur) => acc + cur), "hex");
+            let colors = Buffer.from(json.colors.reduce((acc, cur) => acc + cur), "hex");
             let filler  = Buffer.alloc(768, "LOSPEC");
             let colorList = Buffer.concat([colors, filler], 768);
-            let colorCount = json.colors.length == 256? 
-                Buffer.from("0100", "hex"): 
-                Buffer.concat([Buffer.alloc(1), Buffer.alloc(1, parseInt(json.colors.length, 16))]);
-
-            let colorTable = Buffer.concat([colorList, colorCount, Buffer.alloc(2, 'FF', 'hex')], 772)
+            let colorCount = Buffer.from(json.colors.length.toString(16).padStart(4, 0), "hex");
+            let transparentIndex = Buffer.alloc(2, 'FF', 'hex'); // Not used so put to 0xFFFF
+            let colorTable = Buffer.concat([colorList, colorCount, transparentIndex], 772)
 
             res.send(colorTable);
         }).catch(err => {
