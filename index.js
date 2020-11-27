1cb00fbf532b243a3e30a0b3e7586d185c1ddc96
Julian Webb <git@julianwebb.ca>
2020-11-26 23:40:12 -0800

    Reorganized the logic

--- index.js
+++ index.js
@@ -15,20 +15,16 @@ app.get('/:palette.act', (req, res) => {
     fetch(`https://lospec.com/palette-list/${palette}.json`)
         .then(fetchRes => fetchRes.json())
         .then(json => {
-            
-            if (json.colors.length == 256) {
-                colourCount = Buffer.from("0100", "hex");
-            } else {
-                colourCount = Buffer.concat([Buffer.alloc(1), Buffer.alloc(1, parseInt(json.colors.length, 16))]);
-                
-            }
-            let colours = json.colors.reduce((acc, cur, idx, src) => {
-                return acc + cur
-            })
-            let colourTable = Buffer.concat([Buffer.alloc(colours.length / 2, colours, 'hex'), Buffer.alloc(768, "LOSPEC")], 768)
-            colourTable = Buffer.concat([colourTable, colourCount, Buffer.alloc(2, 'FFFF', 'hex')], 772)
+            let colors = Buffer.alloc(json.colors.length * 3, json.colors.reduce((acc, cur) => acc + cur), "hex");
+            let filler  = Buffer.alloc(768, "LOSPEC");
+            let colorList = Buffer.concat([colors, filler], 768);
+            let colorCount = json.colors.length == 256? 
+                Buffer.from("0100", "hex"): 
+                Buffer.concat([Buffer.alloc(1), Buffer.alloc(1, parseInt(json.colors.length, 16))]);
 
-            res.send(colourTable);
+            let colorTable = Buffer.concat([colorList, colorCount, Buffer.alloc(2, 'FF', 'hex')], 772)
+
+            res.send(colorTable);
         }).catch(err => {
             console.log(err);
             res.json({
